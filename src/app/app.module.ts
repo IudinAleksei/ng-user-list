ceb0d86bdf83f08a784ecfc7c0a5dce64c587597
Aleksei Iudin <aleksei.iudin@stm-labs.ru>
2021-02-03 14:32:17 +0300

    feat: add mocks and interfaces for data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserItemComponent } from './components/user-item/user-item.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserItemComponent,
+    HeaderComponent,
+    FilterComponent,
+    UserListComponent,
+    UserCardComponent,
+    TodoListComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
