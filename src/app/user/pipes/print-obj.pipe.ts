30d95deb845a10681efd20a2f67cee270e5c52bc
Aleksei Iudin <aleksei.iudin@stm-labs.ru>
2021-02-09 10:53:44 +0300

    refactor: add destructurization to print-obj pipe

--- src/app/user/pipes/print-obj.pipe.ts
+++ src/app/user/pipes/print-obj.pipe.ts
@@ -6,11 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PrintObjPipe implements PipeTransform {
 
-  transform(value: IAddress | ICompany): string[] {
-    const input = Object.entries(value);
-    const output = input.map((item) => {
-      const temp = typeof item[1] === 'object' ? this.transform(item[1]) : item[1];
-      return `${item[0]}: ${temp}`;
+  transform(data: IAddress | ICompany): string[] {
+    const input = Object.entries(data);
+    const output = input.map(([key, value]) => {
+      const temp = typeof value === 'object' ? this.transform(value) : value;
+      return `${key}: ${temp}`;
     });
     return output;
   }
